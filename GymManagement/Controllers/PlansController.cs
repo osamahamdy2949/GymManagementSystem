e4defe512ae2f116636f24d3c70cfe0a47b03c5b
Osama <Osama@DESKTOP-VKBP93E>
2026-05-12 17:12:30 +0300

    Add project files.

---
+++ GymManagement/Controllers/PlansController.cs
@@ -0,0 +1,36 @@
+﻿using GymManagement.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagement.Controllers
+{
+    public class PlansController : Controller
+    {
+        private readonly GymDbContext context;
+
+        public PlansController()
+        {
+            context = new GymDbContext();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var Plans = await context.Plans.ToListAsync();
+
+            if(!Plans.Any() )
+                return View();
+
+            return View(Plans);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var plan = await context.Plans.FindAsync(id);
+            if (plan == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
+        }
+    }
+}
---
+++ GymManagement/DbContexts/GymDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GymManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagement.DbContexts
+{
+    public class GymDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=GymManagementDb;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Plan> Plans { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Plan>(new Configurations.PlanConfiguration());
+        }
+    }
+}
